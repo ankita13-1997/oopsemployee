46eab48a888d5aabd46037344efc10588b75cdc0
ankita13-1997 <parhiankita@gmail.com>
2021-02-03 23:11:09 +0530

    welcome to the employee data area

---
+++ EmployeeAttendenceuc2.java
@@ -0,0 +1,21 @@
+package com.oops;
+
+class EmployeeAttendenceuc2 {
+	
+	int full=1;
+    int part=2;
+    
+     
+     // int partDayHour=8;
+    
+      int days=0;
+	void msg()
+	{
+		
+		 
+		System.out.println("welcome to employee main data");
+		
+		
+	}
+
+}
